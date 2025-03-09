ea7f78304e3898122a8d96eb0ba7cfdf41700363
sunny bharne <sunnybharne@sunnys-Mac-mini.local>
2025-03-09 15:10:50 +0200

    updated terraform provider
    Testing terrafrom snippet

--- lua/github_actions_snippets/snippets/terraform-provider.lua
+++ lua/github_actions_snippets/snippets/terraform-provider.lua
@@ -2,8 +2,18 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 
-ls.add_snippets('yaml', {
-  s("test", {
-    t("test")
+ls.add_snippets('terraform', {
+  s("terraform-provider-azure", {
+    t({"terraform {",""}),
+    t({"  required_version = \">= 0.12\"",""}),
+    t({"  required_providers {",""}),
+    t({"    azurerm = {",""}),
+    t({"      source = \"hashicorp/azurerm\"",""}),
+    t({"      version = \">= 1.0.0\"",""}),
+    t({"    }",""}),
+    t({"  }",""}),
+    t({"}",""})
   })
 })
+
+
